0202bd264252ba1f34d5b5d172e0173192bb7b8d
Todo <155129384+Flixgit@users.noreply.github.com>
2024-08-04 11:29:26 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>○ Channel : @Erai_Rawss\n○ Support Group : <a href='https://t.me/+9jo_z2njIbQ3YzM1'>Click here</a></b>",
+            text = f"<b>○ Owner : <a href='httspa://t.me/TodoFactory'>Todo Factory 👻</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>○ Channel : @Erai_Rawss\n○ Support Group : <a href='https://t.me/+9jo_z2njIbQ3YzM1'>Click here</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
